a481e9252454cbf05f34bf48d99eb3ae113a88bf
Trace019 <glowseperate2.0@gmail.com>
2025-05-05 22:47:39 +0800

    Update jollibee.js

--- javascript/jollibee.js
+++ javascript/jollibee.js
@@ -1,14 +1,22 @@
 const initApp = () => {
-    fetch('json-file/jollibee-products.json')
+    fetch('json-file/mcdo-products.json')
     .then(response => response.json())
     .then(data => {
-        productList = data;
-        addDataToHTML();
-        //get from memory
-        if(localStorage.getItem('cart')){
+        // Target a specific JSON ID
+        const specificProducts = data.filter(product => [9, 10, 11, 12].includes(product.id));
+        
+        if (specificProducts.length > 0) {
+            productList = specificProducts; // Use the specific products
+            addDataToHTML();
+        }
+
+        // Get from memory
+        if (localStorage.getItem('cart')) {
+            // Disable filtering of IDs in the JSON when showing the cart
+            productList = data; // Use the entire JSON data.
             carts = JSON.parse(localStorage.getItem('cart'));
             addCartToHTML();
         }
-    })
+    });
 }
 initApp();
